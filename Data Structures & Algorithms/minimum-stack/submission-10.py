5fd4738517868a94da113eb31c882a5ceaa4b6a5
Sheehan Dandapat <sheehandandapat@gmail.com>
2026-07-14 00:04:28 -0400

    Add: minimum-stack - submission-10

---
+++ Data Structures & Algorithms/minimum-stack/submission-10.py
@@ -0,0 +1,20 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minVals = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        val = min(val, self.minVals[-1] if self.minVals else val)
+        self.minVals.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minVals.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minVals[-1]
